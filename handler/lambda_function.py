e61e61506ae8fceab974f0f653748c772493566c
EC2 Default User <miller.nick.c@gmail.com>
2023-01-08 13:16:42 -0800

    make tag into a variable

--- handler/lambda_function.py
+++ handler/lambda_function.py
@@ -4,6 +4,8 @@ echo "Hello World!" >> /home/ec2-user/helloworld.txt
 from the
 pwd >> /home/ec2-user/helloworld.txt
 """
+#Define the tag possessed by the EC2 instances that we want to execute the script on
+tag='Test'
 
 
 def lambda_handler(event, context):
@@ -15,10 +17,13 @@ def lambda_handler(event, context):
     ec2_client = boto3.client("ec2", region_name='us-east-1')
     ssm_client = boto3.client('ssm')
     
-    reservations = ec2_client.describe_instances(Filters=[{'Name': 'tag:Name', 'Values': ['Test']}])['Reservations']
+    #Gather a list of Reservations that contain instances with tag
+    reservations = ec2_client.describe_instances(Filters=[{'Name': 'tag:Name', 'Values': [tag]}])['Reservations']
     
-    
+    #Create a list of instances to execute the shell script within
     exec_list=[]
+    
+    #Iterate through all the instances within the collected resaervations
     for reservation in reservations:
         print("**************")
         for instance in reservation['Instances']:
@@ -34,5 +39,5 @@ def lambda_handler(event, context):
         InstanceIds = exec_list
     )
         
-    # Print the command ID
+    #See the command run on the target instance Ids
     print(response['Command']['Parameters']['commands'])
